5f0ba129d4db6298ad5347804e7ed53b28de5a6c
sergi.vidiella <sergi.vidiella@upc.edu>
2021-05-27 08:08:02 +0200

    Add Heroku dependences

--- src/app/containers/submit/submit.component.ts
+++ src/app/containers/submit/submit.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ApiService } from 'src/app/services/api.service';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ApiService} from 'src/app/services/api.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -11,48 +11,50 @@ import { Router } from '@angular/router';
 })
 export class SubmitComponent {
 
-	urlPattern = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+  urlPattern = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 
-	submitForm = this.formBuilder.group({
-		title: ['', Validators.required],
-		url: ['', Validators.pattern(this.urlPattern)],
-		text: [''],
-	}, {validator: this.atLeastOneRequired});
+  submitForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    url: ['', Validators.pattern(this.urlPattern)],
+    text: [''],
+  }, {validator: this.atLeastOneRequired});
 
-	
 
-	constructor(
-		private formBuilder: FormBuilder,
-		private apiService: ApiService,
-		private router: Router
-	) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {
+  }
 
-	onSubmit() {
-		console.warn(this.submitForm.errors);
-		if (this.submitForm.valid) {
-			console.warn(this.submitForm.value);
-			this.apiService.newSubmission(this.submitForm.value).subscribe(
-				response => {
-					this.router.navigate(['/new']);
-				},
-				err => {
-					if (err.status == 302) {
-						this.router.navigate(['/item', err.error.query[0].substring(49)]);
-					}
-				});
-		}
-	}
+  onSubmit() {
+    console.warn(this.submitForm.errors);
+    if (this.submitForm.valid) {
+      console.warn(this.submitForm.value);
+      this.apiService.newSubmission(this.submitForm.value).subscribe(
+        response => {
+          this.router.navigate(['/new']);
+        },
+        err => {
+          if (err.status === 302) {
+            this.router.navigate(['/item', err.error.query[0].substring(49)]);
+          }
+        });
+    }
+  }
 
-	atLeastOneRequired(group : FormGroup) : {[s:string ]: boolean} {
-		console.log("atLeastOneRequired");
-		if (group) {
-			if (group.controls['url'].value || group.controls['text'].value ) {
-				console.log("url o text");
-				return null;
-			}
-		}
-		console.log("error");
-		return {'error': true};
-	}
+  atLeastOneRequired(control: AbstractControl): { [s: string]: boolean } | null {
+
+    if (control.get('url').pristine && control.get('text').pristine) {
+      return {'missing': true};
+    }
+
+    if (control.get('url').value || control.get('text').value) {
+      return null;
+    } else {
+      return {'missing': true};
+    }
+
+  }
 
 }
